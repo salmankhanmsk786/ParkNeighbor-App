c00cefe300453ceaa5498ade5d8e8f74abf8e431
TehArmen <aadunkin117@gmail.com>
2023-11-29 01:39:21 -0800

    Added List/Map tabs, Google API, autocomplete, lib

--- ParkNeighbor/src/screens/DestinationSearch/index.js
+++ ParkNeighbor/src/screens/DestinationSearch/index.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import { View, TextInput, Text, FlatList, Pressable } from "react-native";
 import styles from './styles.js';
-import Entypo from 'react-native-vector-icons/Entypo.js';
 import { useNavigation } from "@react-navigation/native";
+import SuggestionRow from "./SuggestionRow.js";
 
-
-import searchResults from '../../../assets/data/search';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const DestinationSearchScreen = (props) => {
 
@@ -16,24 +15,34 @@ const DestinationSearchScreen = (props) => {
   return (
     <View style={styles.container}>
       {/* Input component*/}
+
+      <View style={{height: 500}}>
+        <GooglePlacesAutocomplete
+          placeholder='Where are you going?'
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+            navigation.navigate('Guests');
+          }}
+          fetchDetails
+          styles={{
+            textInput: styles.textInput,
+          }}
+          query={{
+            key: 'AIzaSyBxsM19-JeKP0ZmDsXAkmFXB_AW0NIOTFs',
+            language: 'en',
+            types: '(cities)',
+          }}
+          suppressDefaultStyles
+          renderRow={(item) => <SuggestionRow item={item} />}
+        />
+      </View>
+
       <TextInput
         style={styles.textInput}
         placeholder="Where are you going?"
         value={inputText}
         onChangeText={setInputText}
-      />
-
-      {/*List of destination */}
-      <FlatList
-        data={searchResults}
-        renderItem={({ item }) => (
-          <Pressable onPress={() => navigation.navigate('Guests')} style={styles.row}>
-            <View style={styles.iconContainer}>
-              <Entypo name={"location-pin"} size={30} />
-            </View>
-            <Text style={styles.locationText}>{item.description}</Text>
-          </Pressable>
-        )}
       />
     </View>
   )
